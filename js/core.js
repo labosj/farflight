aada91799747bc2ec94d4f62848c952beca3c3fb
Edwin Rodríguez <edwrodrig@gmail.com>
2014-03-30 14:02:26 -0400

    closes #3 - Memory and calculation reductions

--- js/core.js
+++ js/core.js
@@ -1,7 +1,7 @@
 function Camera(canvas) {
   this.position = [0.0, 100.0];
   this.far = 500.0;
-  this.projectedVertexs = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  this.projectedCoords = [[0, 0, 0, 0], [0, 0, 0, 0]];
   this.viewportHeight = canvas.height;
   this.viewportHalfWidth = canvas.width / 2;
 }
@@ -18,27 +18,30 @@ Camera.prototype.setPosition = function(x, y) {
 }
 
 Camera.prototype.projectShape = function(shape) {
-  for ( var i = 0, size = shape.vertexs.length ; i < size ; ++i ) {
-    copyVertex(shape.getVertex(i), this.projectedVertexs[i]);
-    this.projectVertex(this.projectedVertexs[i]);
-  }
-}
+  this.projectedCoords[0][0] = this.projectedCoords[0][1] = shape.dimension[0][0] - this.position[0];
+  this.projectedCoords[0][2] = this.projectedCoords[0][3] = shape.dimension[0][1] - this.position[0];
 
-Camera.prototype.projectVertex = function(vertex) {  
-  vertex[0] -= this.position[0];
-  vertex[1] -= this.position[1];
+  this.projectedCoords[1][0] = this.projectedCoords[1][1] = -this.position[1];
+  this.projectedCoords[1][2] = this.projectedCoords[1][3] = shape.height - this.position[1];
 
-  var scalar = this.far / vertex[2];
+  var scalar = this.far / shape.dimension[1][0];
 
-  vertex[0] *=  scalar;
-  vertex[1] *= -scalar;
-  vertex[2] *=  scalar;
+  this.projectedCoords[0][0] *=  scalar;
+  this.projectedCoords[0][2] *=  scalar;
+  this.projectedCoords[1][0] *= -scalar;
+  this.projectedCoords[1][2] *= -scalar;
+
+  scalar = this.far / shape.dimension[1][1];
+
+  this.projectedCoords[0][1] *=  scalar;
+  this.projectedCoords[0][3] *=  scalar;
+  this.projectedCoords[1][1] *= -scalar;
+  this.projectedCoords[1][3] *= -scalar;
 }
 
 function Drawer(context, camera) {
   this.context = context;
   this.camera = camera;
-  this.edges = [[0, 1], [0, 2], [0, 4], [1, 3], [1, 5], [2, 3], [2, 6], [3, 7], [4, 5], [4, 6], [5, 7], [7, 6]];
   this.offsetX = context.canvas.width  / 2.0;
   this.offsetY = context.canvas.height / 2.0;
   this.backgroundColor = "#000";
@@ -61,21 +64,34 @@ Drawer.prototype.clearScreen = function() {
 Drawer.prototype.drawShape = function(shape) {
   this.camera.projectShape(shape);
 
-  var alpha = (3000 - shape.positionZ) / 3000;
-  if ( shape.positionZ > 3000.0 ) alpha = 0.0;
+  var alpha = (3000 - shape.dimension[1][0]) / 3000;
+  if ( shape.dimension[1][0] > 3000.0 ) alpha = 0.0;
 
   this.context.strokeStyle = shape.color;
   this.context.globalAlpha = alpha;
 
-  for ( var i = 0 ; i < this.edges.length ; ++i )
-    this.drawEdge(this.camera.projectedVertexs[this.edges[i][0]], this.camera.projectedVertexs[this.edges[i][1]]);  
+  this.drawLine(0, 2, 1, 3);
+  this.drawLine(0, 2, 0, 0); 
+  this.drawLine(0, 2, 2, 2); 
+  
+  this.drawLine(2, 0, 3, 1);
+  this.drawLine(2, 0, 0, 0); 
+  this.drawLine(2, 0, 2, 2);
+
+  this.drawLine(3, 3, 1, 3); 
+  this.drawLine(3, 3, 3, 1);  
+  this.drawLine(3, 3, 2, 2);
+
+  this.drawLine(1, 1, 1, 3); 
+  this.drawLine(1, 1, 3, 1); 
+  this.drawLine(1, 1, 0, 0); 
 }
 
-Drawer.prototype.drawEdge = function(vertex1, vertex2) {
+Drawer.prototype.drawLine = function(x1, y1, x2, y2) {
   this.context.beginPath();
-  this.context.moveTo(vertex1[0] + this.offsetX, vertex1[1] + this.offsetY);
-  this.context.lineTo(vertex2[0] + this.offsetX, vertex2[1] + this.offsetY);
-  this.context.stroke();
+  this.context.moveTo(this.camera.projectedCoords[0][x1] + this.offsetX, this.camera.projectedCoords[1][y1] + this.offsetY);
+  this.context.lineTo(this.camera.projectedCoords[0][x2] + this.offsetX, this.camera.projectedCoords[1][y2] + this.offsetY);
+  this.context.stroke();  
 }
 
 Drawer.prototype.drawTitleInfo = function(distance) {
@@ -106,13 +122,13 @@ Drawer.prototype.drawGameOverMessage = function(distance, time) {
   this.context.fillText((distance / 30 >> 0) + ' meters in ' + (time / 100 >> 0) + ' seconds', 400, 300);   
 }
 
-Drawer.prototype.drawInfo = function(distance, time, velocity) {
+Drawer.prototype.drawInfo = function(distance, time, speed) {
   this.context.globalAlpha = 1.0;
   this.context.fillStyle = "yellow";
   this.context.font = '15px monospace';
   this.context.textAlign = 'left';
-  this.context.fillText("Velocity: " + velocity / 0.3 + ' m/s',25,520);
-  this.context.fillText("Time:     " + (time / 100 >> 0) + ' s', 25,540);
+  this.context.fillText("Speed: " + speed / 0.3 + ' m/s',25,520);
+  this.context.fillText("Time:  " + (time / 100 >> 0) + ' s', 25,540);
 
   this.context.textAlign = 'center';
   this.context.fillText("Distance", 400, 25);
@@ -121,65 +137,32 @@ Drawer.prototype.drawInfo = function(distance, time, velocity) {
 }
 
 function Shape() {
-  this.vertexs = [[1.0, 1.0, 1.0], [1.0, 0.0, 1.0], [-1.0, 1.0, 1.0], [-1.0, 0.0, 1.0], [1.0, 1.0, -1.0], [1.0, 0.0, -1.0], [-1.0, 1.0, -1.0], [-1.0, 0.0, -1.0]];
-  this.positionX = 0.0;
-  this.positionZ = 1000.0;
-  this.width = 1.0;
-  this.height = 1.0;
+  this.dimension = [[0.0, 0.0], [0.0, 0.0]];
   this.color = "green";
 }
 
-Shape.prototype.setShape = function(width, height) {
-  this.width = width;
-  this.height = height;
+Shape.prototype.height = 600.0;
+
+Shape.prototype.init = function(posZ, color) {
+  var posX = Math.floor((Math.random() * 1000) - 500);
+  var width = Math.floor((Math.random() * 20.0) + 50.0);
+  this.dimension[0][0] = posX - width;
+  this.dimension[0][1] = posX + width;
+  this.dimension[1][0] = posZ;
+  this.dimension[1][1] = posZ + width * 2.0;
+  this.color = color;
 }
 
-Shape.prototype.getVertex = function(index) {
-  var vertex = []
-  vertex[0] = this.vertexs[index][0] * this.width + this.positionX;
-  vertex[1] = this.vertexs[index][1] * this.height;
-  vertex[2] = this.vertexs[index][2] * this.width + this.positionZ;
-  return vertex;
-}
-
-Shape.prototype.reset = function() {
-  this.positionX = Math.floor((Math.random() * 1000) - 500);
-  this.positionZ += 3000.0;
-  this.setShape(Math.floor((Math.random() * 20.0) + 50.0), 600.0);
-  this.color = getColor();
-}
-
-Shape.prototype.nearest = function() { return this.getVertex(4)[2]; }
-
-Shape.prototype.collideWithPoint = function(x, y) {
-  var maxX = this.getVertex(0)[0];
-  var maxY = this.getVertex(0)[1];
-  var minX = this.getVertex(3)[0];
-  var minY = this.getVertex(3)[1];
-  if ( x < minX ) return false;
-  if ( x > maxX ) return false;
-  if ( y < minY ) return false;
-  if ( y > maxY ) return false;
+Shape.prototype.collideWithPoint = function(x) {
+  if ( x < this.dimension[0][0] ) return false;
+  if ( x > this.dimension[0][1] ) return false;
   return true;
-}
-
-function copyVertex(source, target) {
-  target[0] = source[0];
-  target[1] = source[1];
-  target[2] = source[2];
-}
-
-function getColor() {
-  var color;
-  if ( game.gameState == 1 ) color = (game.currentDistance / 1000) % 360;
-  else color =  Math.floor((Math.random() * 360));
-  return "hsl("+ color +", 100%, 50%)";
 }
 
 function Game() {
   this.currentDistance = 0;
   this.currentTime = 0;
-  this.currentVelocity = 3.0;
+  this.currentSpeed = 3.0;
   this.bestDistance = window.localStorage.getItem("bestScore") || 0;
   this.shapes = [];
   this.gameState = 0; //0: TITLE, 1:GAME, 2:GAMEOVER
@@ -195,20 +178,26 @@ Game.prototype.initShapes = function() {
   var step = 3000.0 / 20;
   for ( var i = 0 ; i < 20 ; i++ ) {
     var shape = new Shape();
-    shape.reset();
-    shape.positionZ = Math.floor(3000.0 - (step * i));
-    shape.color = getColor();
+    shape.init(Math.floor(3000.0 - (step * i)), this.getShapeColor());
     this.shapes[i] = shape;
   } 
+}
+
+Game.prototype.getShapeColor = function() {
+  var color;
+  if ( this.gameState == 1 ) color = (this.currentDistance / 1000) % 360;
+  else color =  Math.floor((Math.random() * 360));
+  return "hsl("+ color +", 100%, 50%)";
 }
 
 Game.prototype.setGameOver = function() {
   this.gameState = 2;
   this.drawer.backgroundColor = "#700";
 
-  if ( this.currentDistance > this.bestDistance )
+  if ( this.currentDistance > this.bestDistance ) {
     this.bestDistance = this.currentDistance;
     window.localStorage.setItem("bestScore", this.bestDistance);
+  }
 }
 
 var game = new Game();
@@ -221,16 +210,16 @@ game.canvas.addEventListener("mousemove", function(event) {
 
 game.canvas.addEventListener("mousedown", function(event) {
   if ( game.gameState == 1 ) {
-    game.currentVelocity += 3.0;
+    game.currentSpeed += 3.0;
   } else if ( game.gameState == 2) {
     game.drawer.setTitle();
     game.gameState = 0;
-    game.currentVelocity = 3.0;
+    game.currentSpeed = 3.0;
   } else {
     game.gameState = 1;
     game.currentDistance = 0;
     game.currentTime = 0;
-    game.currentVelocity = 3.0;
+    game.currentSpeed = 3.0;
   }
 }, false);
 
@@ -243,20 +232,21 @@ setInterval( function() {
   actDelta = actNow - actThen;
   actThen = actNow;
   var timeRatio = (actDelta / 10.0);
-  var currentVelocity = game.currentVelocity * timeRatio;
+  var currentSpeed = game.currentSpeed * timeRatio;
   var shape;
   for ( var i = 0 ; i < game.shapes.length ; i++ ) {
     shape = game.shapes[i];
-    shape.positionZ -= currentVelocity;
-    if ( shape.nearest() < 0.0 ) {
-      if ( game.gameState == 1 && shape.collideWithPoint(game.camera.position[0], game.camera.position[1]) )
+    shape.dimension[1][0] -= currentSpeed;
+    shape.dimension[1][1] -= currentSpeed;
+    if ( shape.dimension[1][0] < 0.0 ) { //shape meets the camera
+      if ( game.gameState == 1 && shape.collideWithPoint(game.camera.position[0]) )
         game.setGameOver();
-      shape.reset();
+        shape.init(3000.0, game.getShapeColor());
     }
   }
   
   if ( game.gameState == 1 ) {
-    game.currentDistance += currentVelocity;
+    game.currentDistance += currentSpeed;
     game.currentTime += timeRatio;
   }
 }, 10);
@@ -276,10 +266,10 @@ function draw() {
       game.drawer.drawShape(game.shapes[i]);
     
     if ( game.gameState == 1 ) {
-      game.drawer.drawInfo(game.currentDistance, game.currentTime, game.currentVelocity);
+      game.drawer.drawInfo(game.currentDistance, game.currentTime, game.currentSpeed);
     } else if ( game.gameState == 2 ) {
-      game.drawer.drawInfo(game.currentDistance, game.currentTime, game.currentVelocity);
-      game.drawer.drawGameOverMessage(game.currentDistance, game.currentTime, game.currentVelocity);
+      game.drawer.drawInfo(game.currentDistance, game.currentTime, game.currentSpeed);
+      game.drawer.drawGameOverMessage(game.currentDistance, game.currentTime, game.currentSpeed);
     } else {
       game.drawer.drawTitleInfo(game.bestDistance);
     }        
